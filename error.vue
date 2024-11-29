514b582d472d96e35147ff8fd2e7509beaf1d755
Thomas Backers <tbackers@proton.me>
2024-11-29 02:02:28 +0100

    Added default template an error handling.

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const error: Ref<NuxtError<unknown> | null | undefined> = useError();
+const handleError = async (): Promise<void> => {
+  await clearError({ redirect: "/" });
+};
+</script>
+
+<template>
+  <NuxtLayout>
+    <div>
+      <h1>{{ error?.statusCode ?? 500 }}</h1>
+      <h2>{{ error?.statusMessage ?? "Internal Server Error" }}</h2>
+      <p>{{ error?.message ?? "Something went wrong." }}</p>
+      <button @click="handleError">back</button>
+    </div>
+  </NuxtLayout>
+</template>
